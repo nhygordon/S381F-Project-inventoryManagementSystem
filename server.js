393d0c3c3f4971e36c9a607915567c6e0c7600c1
flxxxz <117732158+flxxxz@users.noreply.github.com>
2022-11-21 20:08:52 +0800

    this

--- server.js
+++ server.js
@@ -10,18 +10,22 @@ const MongoClient = require('mongodb').MongoClient;
 const ObjectID = require('mongodb').ObjectID;
 
 const mongourl = 'mongodb+srv://Fl:123456abcd!@cluster0.e7oruyd.mongodb.net/?retryWrites=true&w=majority'; 
-const dbName = 'test';
+
+const dbName = 'Library';
 
 const express = require('express');
 const app = express();
 const session = require('cookie-session');
 
+//const bodyParser = require('body-parser');
+
 const { Buffer } = require('safe-buffer');
 
 var users = new Array(
-	{name: "nhy", password: "nhy"},
-    {name: "fl", password: "fl"},
-    {name: "ccy", password: "ccy"}
+    {name: "a", password: ""},
+	{name: "fl", password: ""},
+    {name: "nhy", password: ""},
+    {name: "ccy", password: ""}
 );
 var DOC = {};
 //Main Body
@@ -29,14 +33,16 @@ app.set('view engine', 'ejs');
 app.use(formidable());
 
 //Middleware
+//app.use(bodyParser.json());
 //Cookie
 app.use(session({
     userid: "session",  
     keys: ['th1s!sA5ecretK3y'],
+    //maxAge: 90 * 24 * 60 * 60 * 1000
 }));
 
 //functions
-    //create new inventory docs
+    //create new Library docs
 const createDocument = (db, createDoc, callback) => {
     const client = new MongoClient(mongourl);
     client.connect((err) =>{
@@ -44,7 +50,7 @@ const createDocument = (db, createDoc, callback) => {
         console.log("Connected successfully to the DB server.");
         const db = client.db(dbName);
 
-        db.collection('inventory').insertOne(createDoc, (error, results)=>{
+        db.collection('Library').insertOne(createDoc, (error, results)=>{
             if(error) throw error;
             console.log(results);
             callback();
@@ -53,7 +59,7 @@ const createDocument = (db, createDoc, callback) => {
 }
     //find doc with criteria
 const findDocument = (db, criteria, callback) => {
-    let cursor = db.collection('inventory').find(criteria);
+    let cursor = db.collection('Library').find(criteria);
     console.log(`findDocument: ${JSON.stringify(criteria)}`);
     cursor.toArray((err, docs)=>{
         assert.equal(err, null);
@@ -62,7 +68,7 @@ const findDocument = (db, criteria, callback) => {
     });
 }
 const deleteDocument = (db, criteria, callback) => {
-    db.collection('inventory').deleteOne(
+    db.collection('Library').deleteOne(
        criteria, 
        (err, results) => {
           assert.equal(err, null);
@@ -78,7 +84,7 @@ const updateDocument = (criteria, updateDoc, callback) => {
         console.log("Connected successfully to server");
         const db = client.db(dbName);
 
-        db.collection('inventory').updateOne(criteria,
+        db.collection('Library').updateOne(criteria,
             {
                 $set : updateDoc
             },
@@ -101,7 +107,7 @@ const handle_Find = (req, res, criteria) =>{
         findDocument(db, {}, (docs)=>{
             client.close();
             console.log("Closed DB connection.");
-            res.status(200).render('home', {name: `${req.session.userid}`, ninventory: docs.length, inventory: docs});
+            res.status(200).render('home', {name: `${req.session.userid}`, ninventory: docs.length, Library: docs});
         });
     });
 }
@@ -118,7 +124,7 @@ const handle_Details = (res, criteria) => {
         findDocument(db, DOCID, (docs) => {  
             client.close();
             console.log("Closed DB connection");
-            res.status(200).render('details', {inventory: docs[0]});
+            res.status(200).render('details', {Library: docs[0]});
         });
     });
 }
@@ -141,10 +147,10 @@ const handle_Delete = (res, criteria) =>{
     client.close();
     res.status(200).render('info', {message: "Document successfully deleted."});
 }
-
 //handling requests
 app.get('/', (req, res)=>{
     if(!req.session.authenticated){
+        console.log("...Not authenticated; directing to login");
         res.redirect("/login");
     }
     console.log("...Hello, welcome back");
@@ -156,17 +162,17 @@ app.get('/login', (req, res)=>{
     res.sendFile(__dirname + '/public/login.html');
     res.status(200).render("login");
 });
-app.post('/login', function(req, res) {
+
+app.post('/login', (req, res)=>{
     console.log("...Handling your login request");
-    for (var i=0; i<users.length; i++) {
-		if (users[i].name == req.fields.name &&
-            users[i].password == req.fields.password) {
+    users.forEach((user) => {
+		if (user.name == req.fields.username && user.password == req.fields.password) {
         req.session.authenticated = true;
-        req.session.userid = users[i].username;
+        req.session.userid = req.fields.username;
         console.log(req.session.userid);
         res.status(200).redirect("/home");
         }
-    }
+    });
     res.redirect("/");
 });
 app.use((req, res, next) => {
@@ -178,13 +184,13 @@ app.use((req, res, next) => {
     }
 });
 
-app.get('/logout', function(req, res){
+app.get('/logout', (req, res)=>{
     req.session = null;
     req.authenticated = false;
     res.redirect('/');
 });
 //Home page
-app.get('/home', (req, res) => {
+app.get('/home', (req, res)=>{
     console.log("...Welcome to the home page!")
     const client = new MongoClient(mongourl);
     client.connect((err)=>{
@@ -195,13 +201,218 @@ app.get('/home', (req, res) => {
         findDocument(db, {}, (docs)=>{
             client.close();
             console.log("Closed DB connection.");
-            res.status(200).render('home', {name: `${req.session.userid}`, ninventory: docs.length, inventory: docs});
+            res.status(200).render('home', {name: `${req.session.userid}`, ninventory: docs.length, Library: docs});
         });
     });
     //res.status(200).render('home', {name: `${req.session.userid}`});
 });
+//details
+app.get('/details', (req,res) => {
+    handle_Details(res, req.query);
+});
 
-//not found
+app.get("/map", (req, res) => {
+    console.log("...returning the map leaflet.");
+	res.status(200).render("map", {
+		lat:`${req.query.lat}`,
+		lon:`${req.query.lon}`,
+		zoom:`${req.query.zoom ? req.query.zoom : 15}`
+	});
+});
+
+app.get('/create', (req, res)=>{
+    res.status(200).render("create");
+});
+app.post('/create', (req, res)=>{
+    console.log("...create a new document!");
+    const client = new MongoClient(mongourl);
+    client.connect((err)=>{
+        assert.equal(null, err);
+        console.log("Connected successfully to the DB server.");
+        const db = client.db(dbName);
+                
+// Get a timestamp in seconds
+var timestamp = Math.floor(new Date().getTime()/1000);
+// Create a date with the timestamp
+var timestampDate = new Date(timestamp*1000);
+
+// Create a new ObjectID with a specific timestamp
+var objectId = new ObjectID(timestamp);
+
+	DOC["_id"] = objectId;
+
+        DOC['inv_id']= "";
+        DOC['name']= req.fields.name;
+        DOC['Isbn']= req.fields.Isbn;
+        DOC['quantity']= req.fields.quantity;
+        DOC['owner']= `${req.session.userid}`;
+        console.log("...putting data into DOC");
+        DOC['Publisher'] = req.fields.Publisher;
+		DOC['Brief_Synopsis'] = req.fields.Brief_Synopsis;
+        
+        var pdoc = {};
+        if (req.files.photo && req.files.photo.size > 0 && (pdoc['mimetype'] == 'image/jpeg' || pdoc['mimetype'] == 'image/png')) {
+            fs.readFile(req.files.photo.path, (err, data) => {
+                assert.equal(err,null);
+                pdoc['title'] = req.fields.title;
+                pdoc['data'] = new Buffer.from(data).toString('base64');
+                pdoc['mimetype'] = req.files.photo.type;
+                    
+            });
+        } 
+        DOC['photo'] = pdoc;
+        
+        if(DOC.name &&  DOC.owner){
+            console.log("...Creating the document");
+            createDocument(db, DOC, (docs)=>{
+                client.close();
+                console.log("Closed DB connection");
+                res.status(200).render('info', {message: "Document created successfully!"});
+            });
+        } else{
+            client.close();
+            console.log("Closed DB connection");
+            res.status(200).render('info', {message: "Invalid entry - Name & Owner is compulsory!"});
+        }
+    });
+    client.close();
+    console.log("Closed DB connection");
+    res.status(200).render('info', {message: "Document created"}); 
+});
+
+app.get('/edit', (req, res)=>{
+    console.log("...Enter update page");
+    const client = new MongoClient(mongourl);
+    client.connect((err) => {
+        assert.equal(null, err);
+        console.log("Connected successfully to server");
+        const db = client.db(dbName);
+
+        let DOCID = {};
+        DOCID['_id'] = ObjectID(req.query._id);
+        findDocument(db, DOCID, (docs) => {  
+            client.close();
+            console.log("Closed DB connection");
+            console.log(docs[0]);
+            res.status(200).render('edit', {Library: docs[0]});
+        });
+    });
+
+}); 
+app.post('/update', (req, res)=>{
+    var updateDOC={};
+    console.log("...handle Update");
+    const client = new MongoClient(mongourl);
+        client.connect((err) => {
+            assert.equal(null, err);
+            console.log("Connected successfully to server");
+            console.log("...checking owner");
+            
+            if(req.fields.owner == req.session.userid){
+                if(req.fields.name){
+                updateDOC['name']= req.fields.name;
+                updateDOC['inv_type']= req.fields.inv_type;
+                updateDOC['quantity']= req.fields.quantity;
+                updateDOC['owner']= `${req.session.userid}`;
+                var adoc ={};
+                adoc['building'] = req.fields.building;
+				adoc['country'] = req.fields.country;
+                adoc['street'] = req.fields.street;
+                adoc['zipcode'] = req.fields.zipcode;
+                adoc['coord'] = [req.fields.latitude, req.fields.longitude];
+                updateDOC['address'] = adoc;
+                var DOCID = {};
+                DOCID['_id'] = ObjectID(req.fields._id);
+                if (req.files.photo.size > 0) {
+                    var pdoc = {};
+                    fs.readFile(req.files.photo.path, (err, data) => {
+                        assert.equal(err,null);
+                        pdoc['title'] = req.fields.title;
+                        pdoc['data'] = new Buffer.from(data).toString('base64');
+                        pdoc['mimetype'] = req.files.photo.type;
+                            
+                    });
+                    updateDOC['photo'] = pdoc;
+                    updateDocument(DOCID, updateDOC, (docs) => {
+                        client.close();
+                        console.log("Closed DB connection");
+                        res.status(200).render('info', {message: "Document updated successfully!."});
+                    });
+                }else{
+                    updateDocument(DOCID, updateDOC, (docs) => {
+                        client.close();
+                        console.log("Closed DB connection");
+                        res.status(200).render('info', {message: "Document updated successfully!."});
+                    });
+                }
+            }else{
+                res.status(200).render('info', {message: "Invalid entry - Name is compulsory!"});}
+              
+    }else{
+                res.status(200).render('info', {message: "Invalid owner - Only the owner can update the page!"});
+            }
+    });
+    
+});
+
+app.get('/delete', (req, res)=>{
+    if(req.session.userid == req.query.owner){
+        console.log("...hello owner of the document");
+        handle_Delete(res, req.query);
+    }else{
+        res.status(200).render('info', {message: "Access denied - You don't have the access right!"}); 
+    }
+});
+
+
+//Rest API
+//name
+app.get('/api/Library/name/:name', function(req,res)  {
+    console.log("...Rest Api");
+	console.log("name: " + req.params.name);
+    if (req.params.name) {
+        let criteria = {};
+        criteria['name'] = req.params.name;
+        const client = new MongoClient(mongourl);
+        client.connect((err) => {
+            assert.equal(null, err);
+            console.log("Connected successfully to server");
+            const db = client.db(dbName);
+
+            findDocument(db, criteria, (docs) => {
+                client.close();
+                console.log("Closed DB connection");
+                res.status(200).json(docs);
+            });
+        });
+    } else {
+        res.status(500).json({"error": "missing name"});
+    }
+});
+///inv_type
+app.get('/api/Library/inv_type/:inv_type', (req,res) => {
+    console.log("...Rest Api");
+	console.log("inv_type: " + req.params.inv_type);
+    if (req.params.inv_type) {
+        let criteria = {};
+        criteria['inv_type'] = req.params.inv_type;
+        const client = new MongoClient(mongourl);
+        client.connect((err) => {
+            assert.equal(null, err);
+            console.log("Connected successfully to server");
+            const db = client.db(dbName);
+
+            findDocument(db, criteria, (docs) => {
+                client.close();
+                console.log("Closed DB connection");
+                res.status(200).json(docs);
+            });
+        });
+    } else {
+        res.status(500).json({"error": "missing inv_type"});
+    }
+});
+
 app.get('/*', (req, res)=>{
     res.status(404).render("info", {message: `${req.path} - Unknown request!`})
 });
